401d467f5b367d44677c204820617b9b23f283fe
liuhy <rokkki@163.com>
2023-05-20 11:29:28 +0800

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // 开启外部访问
+    host: '0.0.0.0',
+    // port: 3000,
+    open: true,
+    strictPort: true,
+    https: false,
+    // 配置代理
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
